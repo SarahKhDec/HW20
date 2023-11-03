d6549f9d0735116f476d21b5b16948933c50713d
Sarai <sarahkhanjari@gmail.com>
2023-11-03 07:43:04 +0330

    Applications

---
+++ src/test/java/org/example/HW20/Hw20ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.HW20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
