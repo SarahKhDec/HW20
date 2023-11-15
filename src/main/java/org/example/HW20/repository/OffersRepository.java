8f570c7f1a3dcc92b6b1a3f8c76b0b7dc557a84a
Sarai <sarahkhanjari@gmail.com>
2023-11-15 21:13:51 +0330

    updated OfferRepo

--- src/main/java/org/example/HW20/repository/OffersRepository.java
+++ src/main/java/org/example/HW20/repository/OffersRepository.java
@@ -1,5 +1,6 @@
 package org.example.HW20.repository;
 
+import org.example.HW20.dto.offer.OfferByScorePriceDto;
 import org.example.HW20.entity.Offers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,11 +13,13 @@ import java.util.Optional;
 @Repository
 public interface OffersRepository extends JpaRepository<Offers, Long> {
 
-    @Query("select o from Offers o where o.order.id= ?1 order by o.proposedPrice desc ")
-    List<Offers> findByOrderIdOrderByProposedPrice(Long orderId);
+    @Query("select new org.example.HW20.dto.offer.OfferByScorePriceDto(o.id, o.expert.score, o.expert.email, o.proposedPrice)" +
+            " from Offers o where o.order.id= :orderId order by o.proposedPrice asc ")
+    List<OfferByScorePriceDto> findByOrderIdOrderByProposedPrice(Long orderId);
 
-    @Query("select o from Offers o where o.order.id= ?1 order by o.expert.score desc ")
-    List<Offers> findByOrderIdOrderByExpertScore(Long orderId);
+    @Query("select new org.example.HW20.dto.offer.OfferByScorePriceDto(o.id, o.expert.score, o.expert.email, o.proposedPrice)" +
+            " from Offers o where o.order.id= :orderId order by o.expert.score desc ")
+    List<OfferByScorePriceDto> findByOrderIdOrderByExpertScore(Long orderId);
 
     @Query("select o from Offers o where o.id= ?1 and o.order.id= ?2")
     Optional<Offers> findByIdAndOrderId(Long id, Long orderId);
